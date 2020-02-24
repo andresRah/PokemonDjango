f37db8b3894f09616dd362f993a78c5bb8652b7f
Andres Arevalo <andres.arevalopar@gmail.com>
2020-02-24 00:31:10 -0500

    Add migration and settings

--- PokemonDjango/settings.py
+++ PokemonDjango/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'PokemonDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS']
     }
 }
 
---
+++ env.sh
@@ -0,0 +1,5 @@
+export DBHOST="postgresql-pokemon.postgres.database.azure.com"
+export DBUSER="manager@postgresql-pokemon"
+export DBNAME="pokemontest"
+export DBPASS="supersecretpass"
+
